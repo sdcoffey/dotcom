e579f79d56744e53468dbd6509e61bf49c50c898
sdcoffey <stevendcoffey@gmail.com>
2021-04-12 10:02:04 -0700

    toolbar selection

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -5,22 +5,51 @@ import macLogo from "../../assets/mac-logo.png";
 
 import { Text } from "../Text";
 
-import "./Toolbar.scss";
+import styles from "./Toolbar.module.scss";
 
 const Toolbar: React.FC<Record<string, never>> = () => {
+  const [selectedIndex, setSelectedIndex] = React.useState<number>();
+
+  const toolbarButtonGroup = document.querySelector(`.${styles.toolbargroup}`);
+
+  const onSelect = (i: number) => (event: React.MouseEvent): void => {
+    event.preventDefault();
+    setSelectedIndex(i);
+  };
+
+  const handleClickOutside = (event: MouseEvent): void => {
+    let toolBarContainsClick = false;
+    if (event) {
+      toolBarContainsClick =
+        toolbarButtonGroup?.contains(event.target as Node) || false;
+    }
+
+    if (selectedIndex !== undefined && !toolBarContainsClick) {
+      setSelectedIndex(undefined);
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return (): void => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
   return (
-    <div className="toolbar">
-      <div className="toolbar-group">
-        <ToolbarButton>
-          <img className="mac-logo" height={15} src={macLogo} />
+    <div className={styles.toolbar}>
+      <div className={styles.toolbargroup}>
+        <ToolbarButton onSelect={onSelect(0)} active={selectedIndex === 0}>
+          <img className={styles.maclogo} height={15} src={macLogo} />
         </ToolbarButton>
-        <ToolbarButton>
+        <ToolbarButton onSelect={onSelect(1)} active={selectedIndex === 1}>
           <Text>File</Text>
         </ToolbarButton>
-        <ToolbarButton>
+        <ToolbarButton onSelect={onSelect(2)} active={selectedIndex === 2}>
           <Text>Edit</Text>
         </ToolbarButton>
-        <ToolbarButton>
+        <ToolbarButton onSelect={onSelect(3)} active={selectedIndex === 3}>
           <Text>View</Text>
         </ToolbarButton>
       </div>
--- src/components/Toolbar/ToolbarButton.tsx
+++ src/components/Toolbar/ToolbarButton.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import classnames from "classnames";
 
-import "./ToolbarButton.scss";
+import styles from "./ToolbarButton.module.scss";
 
 interface ToolbarButtonProps {
   children: React.ReactNode;
+  onSelect: (event: React.MouseEvent) => void;
+  active: boolean;
 }
 
 const ToolbarButton: React.FC<ToolbarButtonProps> = (
   props: ToolbarButtonProps
 ): JSX.Element => {
-  const { children } = props;
+  const { children, onSelect, active = false } = props;
 
-  return <button className="ToolbarButton">{children}</button>;
+  const onMouseDown = (event: React.MouseEvent): void => {
+    onSelect(event);
+  };
+
+  const classes = classnames(styles.toolbarbutton, {
+    [styles.active]: active,
+  });
+
+  return (
+    <button onMouseDown={onMouseDown} className={classes}>
+      {children}
+    </button>
+  );
 };
 
 export default ToolbarButton;
